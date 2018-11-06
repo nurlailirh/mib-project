2362fc720695865dab6ca212ab11ac5bb6cc52cd
nurlailirh <nurlailirizkihasanah@hotmail.com>
2018-11-06 14:49:25 +0700

    Update auto-setup.sh

--- auto-setup.sh
+++ auto-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo yum update
 sudo yum upgrade
-sudo yum install python
+sudo apt-get install python
 sudo yum install python-setuptools python-dev build-essential
 sudo easy_install pip
 sudo pip install pip-requirements.txt
